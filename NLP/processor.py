6e48f071ae1825ae32da38e624542036b6c35ee7
Yongtang Lu <wilsonluyongtang_2005@hotmail.com>
2020-03-27 18:29:20 -0400

    Completed the generation of the trace file

--- NLP/processor.py
+++ NLP/processor.py
@@ -1,28 +1,38 @@
 from NLP.countingtable import CountingTable
 from NLP.sentenceparser import SentenceParser
+from decimal import Decimal
 import math
+
 
 class NBCModel:
     def __init__(self, vocabulary_type, ngram_size, smooth_val):
         self.vocabulary = vocabulary_type
         self.ngram_size = ngram_size
         self.smooth_val = smooth_val
-        self.sentence_parser = SentenceParser(vocabulary_type,ngram_size)
+        self.sentence_parser = SentenceParser(vocabulary_type, ngram_size)
         self.counting_table = CountingTable(smooth_val)
 
-    def learnfromfile(self,textfile):
-        with open(textfile,'r', encoding="utf8") as tweets_file:
+    def learnfromfile(self, textfile):
+        with open(textfile, 'r', encoding="utf8") as tweets_file:
             for tweet_line in tweets_file:
-                lang, tweet = self.extractLangAndTweet(tweet_line)
+                lang, tweet, tweet_id = self.extractLangAndTweet(tweet_line)
                 for charsSequence in self.sentence_parser.parseSentence(tweet):
-                    self.counting_table.addCount(charsSequence,lang)
+                    self.counting_table.addCount(charsSequence, lang)
         self.counting_table.addUnknownCount()
         self.counting_table.updateLanguageSumCount()
         pass
 
     def extractLangAndTweet(self, tweet_line):
         tweet_ = tweet_line.split("\t")
-        return tweet_[2],tweet_[3]
+        if 0 <= len(tweet_) < 4:
+            return "", "", ""
+        return tweet_[2], tweet_[3], tweet_[0]
+
+    def extractID(self, tweet_line):
+        tweet_ = tweet_line.split("\t")
+        if 0 <= len(tweet_) < 4:
+            return ""
+        return tweet_[0], tweet_[3]
 
     def classify(self, sentence):
         print()
@@ -33,18 +43,18 @@ class NBCModel:
         best_score_lang = None
         print("scores:")
         for key_lang in self.counting_table.init_languages:
-            score_ = self.score(charsSequenceSet,key_lang)
+            score_ = self.score(charsSequenceSet, key_lang)
             print(key_lang, score_)
             if best_score is None or score_ > best_score:
                 best_score = score_
                 best_score_lang = key_lang
-        return best_score_lang
+        return best_score_lang, '%.2E' % Decimal(best_score)
 
     # score(language)
-    def score(self,charsSequenceSet,language):
-        score_sum = math.log( self.languageProbability(language) )
+    def score(self, charsSequenceSet, language):
+        score_sum = math.log(self.languageProbability(language))
         for charsSequence in charsSequenceSet:
-            conditional_probability_score = self.charSequenceLanguageCP(charsSequence,language)
+            conditional_probability_score = self.charSequenceLanguageCP(charsSequence, language)
             if conditional_probability_score == 0: continue
             score_sum += math.log(conditional_probability_score)
         return score_sum
@@ -53,15 +63,31 @@ class NBCModel:
     def languageProbability(self, language):
         total_languange_count = self.counting_table.total_language_count[language]
         languages_sum_count = self.counting_table.languages_sum_count
-        return total_languange_count/languages_sum_count
+        return total_languange_count / languages_sum_count
 
-    # P(charsSquence|language) CL
+    # P(charsSequence|language) CL
     def charSequenceLanguageCP(self, charsSequence, language):
-        total_chars_sequence_count = self.counting_table.vocabulary_count[charsSequence][language]
+        # total_chars_sequence_count = self.counting_table.vocabulary_count[charsSequence][language]
+        total_chars_sequence_count = self.counting_table.getCount(charsSequence, language)
         total_languange_count = self.counting_table.total_language_count[language]
-        return total_chars_sequence_count/total_languange_count
+        return total_chars_sequence_count / total_languange_count
         pass
 
-    def learn(self, tweets):
+    def predict(self, file):
+        output = ""
+        double_space = "  "
+        with open(file, 'r', encoding="utf8") as tweets_file:
+            for tweet_line in tweets_file:
+                lang, tweet, tweet_id = self.extractLangAndTweet(tweet_line)
+                res, score = self.classify(tweet)
+                compare = "correct" if res == lang else "wrong"
+                output = output + tweet_id + double_space + res + double_space + score + double_space + lang + \
+                    double_space + compare + "\n"
+        self.generate_trace(output)
 
-        pass
+    def generate_trace(self, contents):
+        file_name = "./output/trace_" + str(self.vocabulary) + "_" + str(self.ngram_size) + "_" + str(
+            self.smooth_val) + ".txt"
+        out_file = open(file_name, "w")
+        out_file.writelines(contents)
+        out_file.close()
