4508c586d06622d86089fbb0c198dcf7c2cdb6e9
Yongtang Lu <wilsonluyongtang_2005@hotmail.com>
2020-04-04 13:02:12 -0400

    For testing isAlpha()

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from NLP import processor
+from NLP import sentenceparser
 
-processor = processor.NBCModel(0, 2, 1)
+# processor = sentenceparser.SentenceParser()
+#
+# print(processor.is_in_vocabulary('a'))
+# print(processor.is_in_vocabulary('A'))
+# print(processor.is_in_vocabulary('é'))
+# print(processor.is_in_vocabulary('ÿ'))
 
-print(processor.is_in_vocabulary('a'))
-print(processor.is_in_vocabulary('A'))
-print(processor.is_in_vocabulary('é'))
-print(processor.is_in_vocabulary('ÿ'))
+count = 0
+count2 = 0
+# unicode = 17 planes of 2**16 symbols
+for codepoint in range(17 * 2**16):
+    ch = chr(codepoint)
+    if ch.isalpha():
+        count = count + 1
+    if sentenceparser.SentenceParser.is_52_case_sensitive(ch):
+        count2 = count2 + 1
+print(count, count2)
